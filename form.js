91ad6cbb584e55c3c67507fd14dbbdf03621b412
krayevsky <heartoscope@gmail.com>
2021-11-03 15:38:46 -0600

    Fixed a horrible horrible bug.

--- form.js
+++ form.js
@@ -1,32 +1,42 @@
-// const axios = require("axios");
 
 // const getAllProducts = async () => {
 // 	const res = await axios.get(`http://localhost:3001`);
 // 	console.log(res.data);
 // };
 
-// const getFirstName = document.querySelector("#firstname");
-// const getLastName = document.querySelector("#lastname");
-// const getEmail = document.querySelector("#email");
-// const signUp = document.querySelector("#preorder-form");
+const getFirstName = document.querySelector("#firstname");
+const getLastName = document.querySelector("#lastname");
+const getEmail = document.querySelector("#email");
+const signUp = document.querySelector("#preorder-form");
+const productID = document.querySelector("#productid")
+const timestamp = document.querySelector("#preorderdate")
 
-// const handleSubmitEPA = () => {
-// 	axios.post;
-// };
+// @ts-ignore
+const handleSubmit = async (obj) => {
+	try {
+		await axios.post("http://localhost:3001/preorder", obj)
+		window.alert("Your submissiion")
+	} catch (error) {
+		window.alert("Something went wrong. Please try again.")
+	}
 
-// const firstNameValue = () => {
-// 	console.log("hit");
-// };
-
-// console.log(getFirstName);
-
-// getFirstName.addEventListener("submit", firstNameValue);
-
-const newPage = () => {
-	location.href = "http://locahost:5501/about.html";
-	console.log("I'm trying, okay?");
 };
 
-// const submit = document.getElementById("submit-btn");
-
-// submit.addEventListener("click", newPage);
+signUp.addEventListener('submit', (e) => {
+	e.preventDefault();
+	// @ts-ignore
+	const formValues = {
+	// @ts-ignore
+	productid: +productID.value,
+	// @ts-ignore
+	firstname: getFirstName.value,
+	// @ts-ignore
+	lastname: getLastName.value,
+	// @ts-ignore
+	email: getEmail.value,
+	// @ts-ignore
+	preorderdate: timestamp.value
+	}
+	console.log(formValues)
+	handleSubmit(formValues)
+})
--- server/index.js
+++ server/index.js
@@ -9,8 +9,6 @@ const pool = require("./db.js");
 // Middleware
 app.use(cors());
 app.use(express.json());
-// parse application/x-www-form-urlencoded
-app.use(bodyParser());
 
 // app.get("/", async (req, res) => {
 // 	try {
@@ -24,20 +22,18 @@ app.use(bodyParser());
 // 	}
 // });
 
-app.post("/", (req, res) => {
-	try {
+app.post("/preorder", (req, res) => {
 		const { productid, firstname, lastname, email, preorderdate } = req.body;
 		console.log(req.body);
 		console.log(firstname);
-
+		console.log(productid);
 		const submitForm = pool.query(
+	
 			"INSERT INTO preorders (product_id, first_name, last_name, email, preorder_date) VALUES ($1,$2,$3,$4,$5)",
 			[productid, firstname, lastname, email, preorderdate],
 		);
-		res.status(201);
-	} catch (error) {
-		console.log(error);
-	}
+		res.status(201).json({message:"success"});
+
 });
 
 app.listen(3001, () => console.log("Ready at 3001."));
